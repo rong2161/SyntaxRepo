3364ad1b249a2fbddd74e7c2e5596a1f664ae296
Rong Zhang <rong2161@gmail.com>
2023-04-05 21:20:49 -0400

    My first commit!

---
+++ main.java
@@ -0,0 +1,8 @@
+inport java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+	System.out.println("Yes This is world!");
+    }
+}
